f1d3484a743c28fce0f0a241ebec07ef07761094
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-13 20:46:08 +1000

    finished sec3 lec11 importing npm modules

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-/* require('./utils') returns whatever value we exported from utils.js */
-const add = require('./utils');
+/* Before we can import npm modules in our application, we first have to initialize npm within our application by
+typing this command in the terminal: npm init. It will create a package.json file for us after taking us through the
+steps. When we install an npm package, it creates/amends the package-lock.json and creates/amends the node_modules
+folder with the installed package.
+To specify what version of a package we want to install, we attach '@' to the package name followed by the version
+number e.g. npm i validator@10.8.0. Note: 'i' is shorthand for 'install'.
+At this moment, Node.js does not support ES6 imports (e.g. import validator from 'validator') but it eventually will.*/
+const validator = require('validator');
+
 const getNotes = require('./notes');
 
-const sum = add(3,4);
-
-/* app.js has its own scope with its own variables. The same goes for utils.js. Therefore, app.js cannot access
-utils.js' variables. If we want to get utils.js' variables we have to explicitly export it from utils.js.
- */
-console.log(sum);
 console.log(getNotes());
+console.log(validator.isURL('https/sikandarali.me'));
--- utils.js
+++
@@ -1,7 +0,0 @@
-console.log('utils.js');
-
-const name = 'Ali';
-
-const add = (a,b) => a + b;
-
-module.exports = add;
