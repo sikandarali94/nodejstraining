c10144761444f0a54dc72e328f548cf9eeb11ebc
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-26 18:09:57 +1100

    finished sec6 lec30 call stack, callback queue, and event loop

--- app.js
+++ app.js
@@ -15,7 +15,23 @@ the setTimeout() method to resolve). */
 the order that they resolved. This is because JS first executes the main execution stack first and then looks at the
 event queue to see if any event has fired; if the event has fired (in our case, the setTimeout expiring event) then JS
 will run the code of the callback function associated with that event (in our case, it is the console.log() statements
-inside the callback functions of the setTimeout() methods). */
+inside the callback functions of the setTimeout() methods).
+
+JS has something called a call stack which keeps track of the execution of our program by keeping track of all of the
+functions that are currently running. JS has a main, invisible function called main() and when this invisible function
+is completed, that leaves the call stack empty and the main application has been fully executed. When an event fires in
+Node.js, the callback function associated with the event gets added to something called a callback queue. The job of the
+callback queue is to maintain a list of all of the callback functions that are ready to execute. The event loop looks at
+the call stack first, and if it is empty, then runs items from the callback queue. The way the items in the callback
+queue run is that they are added to the call stack in the order that they entered the queue.
+
+Please note: setTimeout() is not a part of the JS programming language. We are not going to find its definition anywhere
+in the JS spec and V8 has implementation for it. Instead, NodeJS creates an implementation of setTimeout() using C++ and
+provides
+it to our JS scripts to use. When we call setTimeout(), it is really registering an event with NodeJS API along with the
+callback function. While JS is single-threaded, Node behind the scenes uses other threads in C++ to manage our events.
+That's what allows us to continue running our application while asynchronous stuff is happening simultaneously, making
+it non-blocking as it does not block the rest of the app from running. */
 console.log('Starting');
 
 setTimeout(() => {
