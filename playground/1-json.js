06ef1e5160071cc0a656dfa7c0a1b253f00ba018
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-26 15:59:05 +1100

    finished sec6 lec28 section intro: asynchronous node.js

--- playground/1-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const dataBuffer = fs.readFileSync('./1-json.json');
-const dataJSON = dataBuffer.toString();
-const dataParsed = JSON.parse(dataJSON);
-
-const updatedData = { ...dataParsed, name: 'Sikandar', age: '24' };
-const updatedJSONData = JSON.stringify(updatedData);
-fs.writeFileSync('./1-json.json', updatedJSONData);
