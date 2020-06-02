f075f85d2ddad913390f274773301f5c51006778
Sikandar Ali <sikandarali94@hotmail.com>
2020-06-02 23:14:55 +1000

    finished sec6 lec38 callback chaining

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Sydney', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+const location = process.argv[2];
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(data);
-    }
-})
+if (location) {
+    geocode(location, (error, data) => {
+        if (error) {
+            /* By returning the console.log() statement, we are logging to the console as well as coming out of the
+            function. */
+            return console.log('Error', error);
+        }
+
+        forecast(data.longitude, data.latitude, (error, forecastData) => {
+            if (error) {
+                return console.log('Error', error);
+            }
+            console.log(data.location);
+            console.log(forecastData);
+        });
+    });
+} else {
+    console.log('Location was not provided.');
+}
+
+/* Callback chaining is when we chain multiple callbacks to do multiple things in a specific order. */
+
+
