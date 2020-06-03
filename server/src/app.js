12f980d4b2636346f9288e71a8b01411aa51a002
Sikandar Ali <sikandarali94@hotmail.com>
2020-06-03 22:43:47 +1000

    finished sec7 lec47 dynamic pages with templating

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,9 @@
 a cross OS compatible way. */
 const path = require('path');
 const express = require('express');
+/* handlebars is a templating engine that will be able to render dynamic content. We'll be able to easily use and reuse
+little pieces of markup throughout the various pages in our app. The hbs library allows handlebars to be easily
+integrated into the Express framework. */
 
 /* __dirname contains a path to the directory the current script lives in e.g. D:\Coding\Practice\NodeJS\server\src. */
 console.log(__dirname);
@@ -13,12 +16,41 @@ console.log(path.join(__dirname, '../public'));
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+/* Below we are telling Express that the view engine we are using is hbs. Note: we must make sure that we write
+'view engine' exactly because that is an exact setting name in Express. */
+app.set('view engine', 'hbs');
+
 /* In general, the use() method is used to customise our server. In our case, we are going to customise the server to
 serve out the folder where our index.html page lives. express.static() method takes the path to the folder we want to
 serve up. We removed the app.get() method for the root URL because Express goes through our code for each URL until it
 satisfies what it needs; because, the root URL ('') is satisfied by app.use(express.static(publicDirectoryPath));, we
 do not need app.get('', ...); */
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  /* Since we have already configured Express to use handlebars as the view engine, we can just use the render method
+  to render our handlebars template, as shown below. render() method just allows us to render one of our views. Express
+  already has it configured that all the views are in the Views folder, so we just provide the name of the view without
+  the extension. The second argument is an object which contains key-value pairs data that we want inside of our view
+  template. */
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Sikandar Ali'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Sikandar Ali'
+  })
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This is some helpful text.'
+  });
+})
 
 /* The get() method lets us configure what the server should do when someone tries to get the resource at a specific
 URL. The callback method is called with two very important arguments. The first is an object containing information
