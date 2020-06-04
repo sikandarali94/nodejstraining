680d37a670949fbcaf9bd32ec926ba1640fdbc01
Sikandar Ali <sikandarali94@hotmail.com>
2020-06-04 16:32:51 +1000

    finished sec8 lec58 creating a search form

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,13 +1,20 @@
-/* The fetch() method is a JS method used to fetch a resource. */
-fetch('http://localhost:3000/weather?address=Boston').then(response => {
-  /* The json() method of the Body mixin takes a Response stream and reads it to completion. It returns a promise that
-  resolves with the result of parsing the body text as JSON and returning it as a JS object. */
-  response.json().then(({ error, forecast, location } = {}) => {
-    if (error) {
-      return console.log(error);
-    }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
-    console.log('Location:', location);
-    console.log('Forecast:', forecast);
-  })
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    /* The json() method of the Body mixin takes a Response stream and reads it to completion. It returns a promise that
+    resolves with the result of parsing the body text as JSON and returning it as a JS object. */
+    response.json().then(({ error, forecast, location } = {}) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Location:', location);
+      console.log('Forecast:', forecast);
+    })
+  });
 });
