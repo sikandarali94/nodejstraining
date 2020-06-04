63bec52f75f012d8dcad3d88aa2ad65d3c2e3293
Sikandar Ali <sikandarali94@hotmail.com>
2020-06-04 16:02:25 +1000

    finished sec8 lec57 browser http requests with fetch

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,1 +1,13 @@
-console.log('Client side javascript file is loaded!');
+/* The fetch() method is a JS method used to fetch a resource. */
+fetch('http://localhost:3000/weather?address=Boston').then(response => {
+  /* The json() method of the Body mixin takes a Response stream and reads it to completion. It returns a promise that
+  resolves with the result of parsing the body text as JSON and returning it as a JS object. */
+  response.json().then(({ error, forecast, location } = {}) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Location:', location);
+    console.log('Forecast:', forecast);
+  })
+});
