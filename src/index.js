2ae533ab0afb40ae80259499eb14ed9a9b5aa9de
Sikandar Ali <sikandarali94@hotmail.com>
2020-06-08 11:50:50 +1000

    finished sec11 lec89 resource creation endpoints: part i

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/* app.use(express.json()); automatically will parse incoming JSON to an object so we can access it in our request
+handlers. */
+app.use(express.json());
+
+/* .post() method is used for the POST HTTP method configuration. There is also .get(), .patch() and .delete()
+methods. */
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+
+  user.save().then(() => {
+    res.status(201).send(user);
+  }).catch(error => {
+    /* We set the status code response using the status method. We must make sure to set the status code before we send
+    out the response. */
+    res.status(400).send(error);
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
+});
